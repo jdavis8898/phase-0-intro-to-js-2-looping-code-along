3495b36ea046bb7e004de4a77269f9e4dc09ce4e
Jeffrey Davis <j.davis081248@gmail.com>
2023-10-27 03:33:19 -0500

    Looping Lab Completed

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function writeCards(nameList, eventName)
+{
+    const messages = [];
+
+    for (let i = 0; i < nameList.length; i++)
+    {
+        messages[i] = `Thank you, ${nameList[i]}, for the wonderful ${eventName} gift!`
+    }
+
+    return messages;
+}
+
+function countDown(x)
+{
+    let num = x;
+    while (num >= 0)
+    {
+        console.log(num);
+        num--;
+    }
+}
